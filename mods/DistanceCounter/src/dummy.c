92a3a1bef3afe3507502d0a6b1ffaeb3b79a5220
RayCarrot <33812678+RayCarrot@users.noreply.github.com>
2023-02-28 10:40:39 +0100

    Remove old files from distance counter mod

--- mods/DistanceCounter/src/dummy.c
+++
@@ -1,6 +0,0 @@
-// A dummy function. Used to have FntPrint return and make its data overwritable.
-// This function is known to take up 8 bytes, so we can start overwriting after that.
-void dummy()
-{
-    return;
-}
