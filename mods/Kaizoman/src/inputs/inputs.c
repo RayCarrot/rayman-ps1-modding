481d55b2ee695a5b6baffcfdcbdf7dab05c954e8
RayCarrot <33812678+RayCarrot@users.noreply.github.com>
2023-03-06 16:15:44 +0100

    Fix Kaizoman death button music and zombie mode

--- mods/Kaizoman/src/inputs/inputs.c
+++ mods/Kaizoman/src/inputs/inputs.c
@@ -6,10 +6,11 @@ extern Obj ray;
 void do_inputs()
 {
     // Instant death when pressing triangle and paused
-    if (in_pause && TOUCHE(INPUT_TRIANGLE))
+    if (in_pause && TOUCHE(INPUT_TRIANGLE) && (ray.flags & 0xc00) == 0xc00)
     {
         ray.hit_points = 0;
         RAY_HIT(1, (Obj *)0x00);
         in_pause = 0;
+        PS1_OnPauseOff();
     }
 }
