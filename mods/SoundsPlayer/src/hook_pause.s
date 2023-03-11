03e14861277532b614049b5c51ba54e55e6e91ef
RayCarrot <33812678+RayCarrot@users.noreply.github.com>
2023-03-11 13:18:01 +0100

    Basic mod for playing/testing in-game sounds

---
+++ mods/SoundsPlayer/src/hook_pause.s
@@ -0,0 +1,2 @@
+.set noreorder
+jal DO_PLAYER
---
+++ mods/SoundsPlayer/src/player.c
@@ -0,0 +1,26 @@
+#include <export.h>
+
+byte sound;
+
+void DO_PLAYER()
+{
+    char sndStr[4];
+
+    display_text("/select sound to play/", 0xA0, 0x50, 1, 1);
+ 
+    atoi(sound, sndStr);
+    display_text(sndStr, 0xA0, 0x78, 1, 1);
+    
+    if (PS1_SpecialTOUCHE(INPUT_RIGHT))
+        sound++;
+    else if (PS1_SpecialTOUCHE(INPUT_LEFT))
+        sound--;
+    else if (PS1_SpecialTOUCHE(INPUT_UP))
+        sound += 10;
+    else if (PS1_SpecialTOUCHE(INPUT_DOWN))
+        sound -= 10;
+    else if (PS1_SpecialTOUCHE(INPUT_CROSS))
+        PlaySnd_old(sound);
+    else if (PS1_SpecialTOUCHE(INPUT_CIRCLE))
+        stop_all_snd();
+}
